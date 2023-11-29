7bdb10078338c3fc45e6f704c704204facf4b9c1
dariabushueva <bushueva84@yandex.ru>
2023-11-29 15:14:04 +0300

    completed app users

--- edu_modules/apps.py
+++ edu_modules/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class EduModulesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'edu_modules'
+    verbose_name = 'Образовательные модули'
---
+++ edu_modules/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from rest_framework.routers import DefaultRouter
+
+from edu_modules.apps import EduModulesConfig
+from edu_modules.views import ModuleViewSet
+
+app_name = EduModulesConfig.name
+
+router = DefaultRouter()
+router.register(r'module', ModuleViewSet, basename='module')
+
+urlpatterns = [] + router.urls
